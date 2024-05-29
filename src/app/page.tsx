ee0be5ec0b68f4b6dbde247417beb108633aa2f8
davidleond99 <84214167+davidleond99@users.noreply.github.com>
2024-05-28 20:15:56 -0400

    Added HomePage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,27 @@
 import { Button } from "@/components";
+import { font } from "@/config/font";
+import { cn } from "@/lib/utils";
 
 export default function Home() {
   return (
-    <p className="font-bold text-green-500">
-      <Button>Click Me</Button>
-    </p>
+    <main
+      className="flex h-full flex-col items-center justify-center 
+    bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-500"
+    >
+      <div className="space-y-6 text-center">
+        <h1
+          className={cn(
+            "text-4xl text-white font-semibold drop-shadow-xl",
+            font.className
+          )}
+        >
+          🔐Auth
+        </h1>
+        <p className="text-white text-lg">A simple authentication service</p>
+        <Button variant="secondary" size={"lg"}>
+          Sign In
+        </Button>
+      </div>
+    </main>
   );
 }
---
+++ src/config/font.ts
@@ -0,0 +1,8 @@
+import { Inter, Poppins } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+
+export const font = Poppins({
+  subsets: ["latin"],
+  weight: ["600"],
+});
