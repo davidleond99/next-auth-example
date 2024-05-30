ffc97bb2d166a07868bf716d23c5bcfd704f17ec
davidleond99 <84214167+davidleond99@users.noreply.github.com>
2024-05-30 14:42:18 -0400

    Impl middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,37 @@
-import { auth } from "./auth";
+import authConfig from "@/auth.config";
+import NextAuth from "next-auth";
+import {
+  apiAuthPrefix,
+  authRoutes,
+  DEFAULT_LOGIN_REDIRECT,
+  publicRoutes,
+} from "@/routes";
+
+const { auth } = NextAuth(authConfig);
 
 export default auth(async (req) => {
+  const { nextUrl } = req;
+
   const isLoggedIn = !!req.auth;
 
-  console.log("HERE ROUTE:", req.nextUrl.pathname);
-  console.log("isLoggedIn:", isLoggedIn);
+  const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+  const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+  const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+
+  if (isApiAuthRoute) return;
+
+  if (isAuthRoute) {
+    if (isLoggedIn) {
+      return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+    }
+    return;
+  }
+
+  if (!isLoggedIn && !isPublicRoute) {
+    return Response.redirect(new URL("/auth/login", nextUrl));
+  }
+
+  return;
 });
 
 export const config = {
